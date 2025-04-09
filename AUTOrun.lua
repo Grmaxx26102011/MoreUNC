5545a4537b002652f225752cdd440a0aa7402636
Grmaxx26102011 <85898714+Grmaxx26102011@users.noreply.github.com>
2025-04-09 13:49:06 +0200

    Update AUTOrun.lua

--- AUTOrun.lua
+++ AUTOrun.lua
@@ -1,1 +1,7 @@
-print("cloudy is dumb LOOLOLOLOLOLOLOLOL")
+print("More UNC loaded")
+getfenv(0).hookfunction(original, replacement)
+    local fakeRef = function() return true end  -- Original always returns true
+    original = function() return false end      -- Hooked version returns false
+    return fakeRef                              -- Return a distinct reference
+end
+local hookfunction = getfenv(0).hookfunction
