67ba7146101c135eea1d897ca22791eb142bb17b
Grmaxx26102011 <85898714+Grmaxx26102011@users.noreply.github.com>
2025-04-09 13:52:02 +0200

    Update AUTOrun.lua

--- AUTOrun.lua
+++ AUTOrun.lua
@@ -1,7 +1,6 @@
 print("More UNC loaded")
-getfenv(0).hookfunction(original, replacement)
+local function hookfunction(original, replacement)
     local fakeRef = function() return true end  -- Original always returns true
     original = function() return false end      -- Hooked version returns false
     return fakeRef                              -- Return a distinct reference
 end
-local hookfunction = getfenv(0).hookfunction
